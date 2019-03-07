d173c1ef2967d4532cd0c63f200ddc18895e85b8
hotstealer <karl.podra@khk.ee>
2019-03-07 09:21:44 +0200

    Lesson 2 writing test

--- add.js
+++ add.js
@@ -1,11 +1,2 @@
-function add(x, y){
-    var result;
-    if (typeof x && typeof y) !== 'number'){
-        throw new Error('Params must be a number.');
-    }
-    result = x + y;
-    if parseInt(result) !== (result) {
-        result = parseFloat(result.toFixed(1));
-    }
-    return result;
-}
+expect(add(0.1, 0.2)).toBe(0.3);
+expect(add(0.1, 0.2)).not.toBe(0.1);
